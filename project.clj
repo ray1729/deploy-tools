9f0d3b9c8a6717dbffe9721b2efc650a719b152b
Ray Miller <ray@metail.com>
2014-02-21 15:33:27 +0000

    Add a version option to the archive-name fn.

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject org.immutant/deploy-tools "0.14.1-SNAPSHOT"
+(defproject org.clojars.ray1729/deploy-tools "0.14.1-SNAPSHOT"
   :description "Handy dandy tools for deploying and archiving Immutant applications."
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :url "https://github.com/immutant/deploy-tools"
   :profiles {:dev {:dependencies [[org.clojure/clojure "1.4.0"]]
                    :resource-paths ["resources" "test-resources"]}}
-  :signing {:gpg-key "BFC757F9"}
+  :signing {:gpg-key "B009089F"}
   :lein-release {:deploy-via :clojars})
