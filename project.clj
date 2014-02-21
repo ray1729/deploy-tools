830caf2673454ac5855434bab33e3bf9e2e54cf4
Ray Miller <ray@metail.com>
2014-02-21 15:52:06 +0000

    Update project :url

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
   :description "Handy dandy tools for deploying and archiving Immutant applications."
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :url "https://github.com/immutant/deploy-tools"
+  :url "https://github.com/ray1729/deploy-tools"
   :profiles {:dev {:dependencies [[org.clojure/clojure "1.4.0"]]
                    :resource-paths ["resources" "test-resources"]}}
   :signing {:gpg-key "B009089F"}
